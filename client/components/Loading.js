2f4bfeb14bdae75685f648b62542542de4cfa683
ayushgupta0010 <ayushgupta0010@gmail.com>
2022-03-06 14:40:23 +0530

    Style Loading component

--- client/components/Loading.js
+++ client/components/Loading.js
@@ -1,7 +1,21 @@
 import React from "react";
 
-const Loading = () => {
-  return <div>Loading...</div>;
-};
+const Loading = () => (
+  <div className='container'>
+    <div
+      className='d-flex justify-content-center align-items-center'
+      style={{ height: "100vh" }}>
+      <div className='spinner-grow text-primary mx-3' role='status'>
+        <span className='visually-hidden'>Loading...</span>
+      </div>
+      <div className='spinner-grow text-primary mx-3' role='status'>
+        <span className='visually-hidden'>Loading...</span>
+      </div>
+      <div className='spinner-grow text-primary mx-3' role='status'>
+        <span className='visually-hidden'>Loading...</span>
+      </div>
+    </div>
+  </div>
+);
 
 export default Loading;
